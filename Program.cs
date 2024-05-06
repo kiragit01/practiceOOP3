383f609691f5798fff8e8a7c34b23974a3286ec4
ksele <ksele@KIRAWIN>
2024-05-06 21:28:35 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace practiceOOP3
+{
+    class Program
+    {
+        static void Main()
+        {
+            Time time = Edit();
+            GC.Collect();
+            Memo(time);
+            Console.ReadKey();
+        }
+
+
+        static Time Edit()
+        {
+            Console.Write("Часов = ");
+            int hour = int.Parse(Console.ReadLine());
+            Console.Write("Минут = ");
+            int minutes = int.Parse(Console.ReadLine());
+            Console.Write("Секунд = ");
+            int sedonds = int.Parse(Console.ReadLine());
+            return new Time(hour, minutes, sedonds);
+
+        }
+
+
+        static void Memo(Time time)
+        {
+            Console.WriteLine(time.СalculateMinutes());
+            Console.WriteLine(time.MinusTenMinutes());
+        }
+    }
+}
---
+++ Time.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace practiceOOP3
+{
+    class Time
+    {
+        public int Hours { get; set; }
+        public int Minutes { get; set; }
+        public int Seconds { get; set; }
+
+        public Time() { }
+
+        public Time(int hours) => Hours = hours;
+
+        public Time(int hours, int minutes)
+        {
+            Hours = hours;
+            Minutes = minutes;
+        }
+
+        public Time(int hours, int minutes, int seconds)
+        {
+            Hours = hours;
+            Minutes = minutes;
+            Seconds = seconds;
+        }
+
+        public string СalculateMinutes()
+        {
+            return $"Всего минут = {Minutes + (Hours * 60) + (Seconds / 60)}";
+        }
+
+        public string MinusTenMinutes()
+        {
+            return $"Меньше на 10 минут = {(Minutes + (Hours * 60) + (Seconds / 60)) - 10}";
+        }
+
+        ~Time()
+        {
+            Console.WriteLine("Информация о времени была удалена.");
+        }
+
+    }
+}
